2d335a3ed46d40f95dfd61b359653354704dae6e
Kelvin Ronaldo <kelvin.ronaldo@hotmail.com>
2020-01-15 23:23:00 -0300

    Inicio da aplicação web

---
+++ web/src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './global.css';
+
+// Componente - bloco isolado de HTML, CSS e JS, o qual não intefere no restante da aplicação
+// Propriedade: Informações que um componente PAI passa para o componente FILHO
+// Estado: Informações mantidas pelo componente (Lembrar: imutabilidade)
+
+function App() {
+	return (
+		<div id="app">
+			<aside>
+				<strong>Cadastrar</strong>
+				<form>
+					<label htmlFor=""></label>
+				</form>
+			</aside>
+			<main>
+
+			</main>
+		</div>
+	);
+}
+
+export default App;
