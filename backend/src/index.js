6b1e51edd6794057939178657ae77ef075924581
Kelvin Ronaldo <kelvin.ronaldo@hotmail.com>
2020-02-02 15:23:53 -0300

    Plataforma web pronto

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,14 +1,17 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const routes = require("./routes");
+const cors = require("cors");
 
 const app = express();
 
-mongoose.connect("mongodb+srv://kelvinOmniStack:omnistack@cluster0-dank0.mongodb.net/week10?retryWrites=true&w=majority", {
+mongoose.connect("mongodb+srv://kelvinOmniStack:omnistack@cluster0-dank0.mongodb.net/week10?retryWrites=true&w=majority",
+{
 	useNewUrlParser: true,
 	 useUnifiedTopology: true
 });
 
+app.use(cors());
 app.use(express.json()); //sempre vem antes das rotas
 app.use(routes);
 
--- web/src/App.js
+++ web/src/App.js
@@ -1,4 +1,7 @@
-import React,  { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
+import api from './services/api';
+import DevItem from "./components/DevItem";
+import DevForm from "./components/DevForm";
 import './global.css';
 import './App.css';
 import './Sidebar.css';
@@ -10,67 +13,34 @@ import './Main.css';
 
 function App() {
 
-	useEffect(() =>{
-		navigator.geolocation.getCurrentPosition(
-			(position) => {
-				const {latitude, longitude} = position.coords;
+	const [devs, setDevs] = useState([]);
 
-				
-			},
-			(error) =>{
-				console.log(error);
-			},
-			{
-				timeout: 30000
-			}
-		)
+	useEffect(()=>{
+		async function loadDevs(){
+			const response = await api.get("/devs")
+			setDevs(response.data);
+		}
+		loadDevs();
 	}, []);
 
 
+	async function handleAddDev(data){
+		const response = await api.post("/devs", data);
+
+		setDevs([...devs, response.data]);
+	}
 
 	return (
 		<div id="app">
 			<aside>
 				<strong>Cadastrar</strong>
-				<form>
-					<div className="input-block">
-						<label htmlFor="username-github">Usuário do GitHub</label>
-						<input name="github_username" id="username-github" required/>
-					</div>
-
-					<div className="input-block">
-						<label htmlFor="techs">Tecnologias</label>
-						<input name="techs" id="techs" required/>
-					</div>
-
-					<div className="input-group">
-						<div className="input-block">
-							<label htmlFor="latitude">Latitude</label>
-							<input name="latitude" id="latitude" required/>
-						</div>
-
-						<div className="input-block">
-							<label htmlFor="longitude">Longitude</label>
-							<input name="longitude" id="longitude" required/>
-						</div>
-					</div>
-
-					<button type="submit">Salvar</button>
-				</form>
+				<DevForm onSubmit={handleAddDev}/>
 			</aside>
 			<main>
 				<ul>
-					<li className="dev-item">
-						<header>
-							<img src="https://avatars0.githubusercontent.com/u/40304152?s=460&v=4" alt="Kelvin Ronaldo"/>
-							<div className="user-info">
-								<strong>Kelvin Ronaldo</strong>
-								<span>Java, ReactJS, C</span>
-							</div>
-						</header>
-						<p>Desenvolvedor de Softwares e entusiasta da tecnologia. Amo inovações em qualquer que seja a área e também curto jogar videogames de vez em quando! XD</p>
-						<a href="https://github.com/KelvinRonaldo">Acessar Perfil no GitHub</a>
-					</li>
+					{devs.map(dev => (
+						<DevItem key={dev._id} dev={dev}/>
+					))}
 				</ul>
 			</main>
 		</div>
